2a9743755e3aad04d5d0f6613d43ea27ce079c45
vearse <versatilekaywize94@gmail.com>
2022-09-15 02:04:13 +0100

    Tournament Modelling

---
+++ database/migrations/2022_09_14_235602_create_match_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_records', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tournament_id');
+            $table->dateTime('period');
+            $table->string('home_team')->nulluable();
+            $table->string('away_team')->nulluable();
+            $table->string('city')->nulluable();
+            $table->string('stadium')->nulluable();
+            $table->integer('round')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /** 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_records');
+    }
+};
