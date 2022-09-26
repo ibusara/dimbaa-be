d20e9dd6831f57419c59a7b572fe63ce5af21266
vearse <versatilekaywize94@gmail.com>
2022-09-26 05:48:44 +0100

    Team Management Completion

---
+++ database/migrations/2022_09_25_100719_create_lineup_forms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lineup_forms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id'); 
+            $table->unsignedBigInteger('competition_id'); 
+            $table->date('detail_date')->nullable();
+            $table->integer('game_no')->nullable(); 
+
+            $table->date('today_date')->nullable();
+            $table->string('manager_signature')->nullable();
+            $table->string('team_signature')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lineup_forms');
+    }
+};
