1725934573997684a5282780d6d44f11c699796e
vearse <versatilekaywize94@gmail.com>
2022-10-14 08:25:00 +0100

    Refree Evaluations

---
+++ app/Http/Controllers/API/Officials/ReferringController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API\Officials;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\ReferringTeam;
+use App\Models\MatchCordinationDetail;
+use App\Models\Notification;
+use Validator;
+use App\Http\Controllers\API\BaseController as BaseController;
+
+class ReferringController extends BaseController
+{
+    public function referringTeam(Request $request){
+        $user = $request->user();
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'match' => 'required|exists:match_records,id',
+            'refree' => 'nullable|array',
+            'refree.user' => 'required|integer',
+            'refree.region' => 'string|max:512',
+            'ass_referee_1' => 'nullable|array',
+            'ass_referee_1.user' => 'required|integer',
+            'ass_referee_1.region' => 'string|max:512',
+            'ass_referee_2' => 'nullable|array',
+            'ass_referee_2.user' => 'required|integer',
+            'ass_referee_2.region' => 'string|max:512',
+            'fourth_official' => 'nullable|array',
+            'fourth_official.user' => 'required|integer',
+            'fourth_official.region' => 'string|max:512',
+
+            'add_referee_one' => 'nullable|array',
+            'add_referee_one.user' => 'required|integer',
+            'add_referee_one.region' => 'string|max:512',
+            'add_referee_two' => 'nullable|array',
+            'add_referee_two.user' => 'required|integer',
+            'add_referee_two.region' => 'string|max:512',
+
+            'additional_assistant_referee_1' => 'nullable|array',
+            'additional_assistant_referee_1.user' => 'required|integer',
+            'additional_assistant_referee_1.region' => 'string|max:512',
+            'additional_assistant_referee_2' =>'nullable|array',
+            'additional_assistant_referee_2.user' => 'required|integer',
+            'additional_assistant_referee_2.region' => 'string|max:512'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $referringTeam = ReferringTeam::firstOrCreate(['match_id' => $input['match']]);
+        $referringTeam->update($input);
+
+        $notification =  new Notification();
+        $notification->role_id = $user->id;
+        $notification->action = '/';
+        $notification->title = "Referee Assessor";
+        $notification->category = "assessor";
+        $notification->description = "Referee Assessor";
+        $notification->save();
+
+        return $this->sendResponse($referringTeam, 'Record is created');
+    }
+}
