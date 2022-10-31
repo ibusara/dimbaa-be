9bff9e39deb2864c8fc50bab5388b6407c7dbc4f
waihura <waihura.m@gmail.com>
2022-10-31 17:07:29 +0300

    Authentication changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use LaratrustUserTrait;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
