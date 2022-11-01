ba2b52ba872ba3a534069e50514ae202fd53f843
waihura <waihura.m@gmail.com>
2022-11-01 14:16:53 +0300

    stadium factory seeder

---
+++ database/factories/StadiumFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Stadium;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stadium>
+ */
+class StadiumFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Stadium::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'region' => $this->faker->text,
+            'location' => $this->faker->name,
+            'capacity' => $this->faker->number,
+            'stadium_owner' => $this->faker->name,
+            'stadium_picture' => $this->faker->image('public/storage/images', 640, 480, null, false),
+            'inauguration_date' => $this->faker->date('Y-m-d', $max = now())
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleAndPermissionSeeder::class,
+            StadiumSeeder::class
         ]);
     }
 }
