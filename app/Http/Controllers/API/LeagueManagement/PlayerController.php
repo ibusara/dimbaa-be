069f73955edc95d1f4cb6de122765775cf6c4c4a
waihura <waihura.m@gmail.com>
2022-11-01 15:42:53 +0300

    Team api fix

--- app/Http/Controllers/API/LeagueManagement/PlayerController.php
+++ app/Http/Controllers/API/LeagueManagement/PlayerController.php
@@ -31,7 +31,6 @@ class PlayerController  extends Controller
         ], 200);
     }
 
-
     /**
      * Create new Player.
      *
@@ -41,7 +40,7 @@ class PlayerController  extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'team_id' => 'required|integer|exists,teams,id',
+            'team_id' => 'required|integer|exists:teams,id',
             'first_name' => 'required|string',
             'middle_name' => 'required|string',
             'last_name' => 'required|string',
@@ -78,7 +77,11 @@ class PlayerController  extends Controller
             'rank' => $request->rank
         ]);
 
-        return response()->json($player, 'Player created successfully.');
+        return response()->json([
+            'success' => true,
+            'message' => 'Player created successfully!',
+            'player' => $player
+        ], 200);
     }
 
     /**
@@ -101,21 +104,49 @@ class PlayerController  extends Controller
      */
     public function update(Request $request, Player $player)
     {
-        $user = $request->user();
-        $input = $request->all();
-
         $request->validate([
-            'team' => 'required|integer', //exists,team,id
-            'name' => 'required|between:3,50',
-            'number' => 'required|integer|between:1,999',
+            'team_id' => 'required|integer|exists:teams,id',
+            'first_name' => 'required|string',
+            'middle_name' => 'required|string',
+            'last_name' => 'required|string',
+            'local_id' => 'required|integer',
+            'fifa_id' => 'required|integer',
+            'playing_position' => 'required|integer',
+            'weight' => 'required',
+            'height' => 'required',
+            'nationality' => 'required',
+            'dob' => 'required|date_format:YYYY-MM-DD',
+            'professional_date' => 'required|date_format:YYYY-MM-DD',
+            'jersey_number' => 'required|integer',
+            'license_no' => 'nullable',
+            'rank' => 'nullable',
         ]);
 
+        $signature = strtoupper(substr($request->first_name, 0, 1) . '.' . substr($request->middle_name, 0, 1)) . '.' . ucwords($request->last_name);
+        $player->update([
+            'team_id' => $request->team_id,
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'local_id' => $request->local_id,
+            'fifa_id'  => $request->fifa_id,
+            'playing_position' => $request->playing_position,
+            'weight' => $request->weight,
+            'height' => $request->height,
+            'nationality' => $request->nationality,
+            'dob' => $request->dob,
+            'professional_date' => $request->professional_date,
+            'jersey_number' => $request->jersey_number,
+            'signature' => $signature,
+            'license_no' => $request->license_no,
+            'rank' => $request->rank
+        ]);
 
-
-        $input['team_id'] = $request->team;
-        $player = $player->update($input);
-
-        return response()->json($player, 'Player updated successfully.');
+        return response()->json([
+            'success' => true,
+            'message' => 'Player updated successfully!',
+            'player' => $player
+        ], 200);
     }
 
     /**
@@ -128,6 +159,9 @@ class PlayerController  extends Controller
     {
         $player->delete();
 
-        return response()->json([], 'Player deleted successfully.');
+        return response()->json([
+            'success' => true,
+            'message' => 'Player deleted successfully!'
+        ], 200);
     }
 }
