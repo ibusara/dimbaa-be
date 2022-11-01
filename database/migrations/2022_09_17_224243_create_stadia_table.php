3655aa6e200b653cfad5cb04306bef519878e675
waihura <waihura.m@gmail.com>
2022-11-01 14:33:09 +0300

    faker update

--- database/migrations/2022_09_17_224243_create_stadia_table.php
+++ database/migrations/2022_09_17_224243_create_stadia_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('stadia', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
             $table->string('name', 100);
             $table->string('region', 100);
             $table->string('location', 100);
