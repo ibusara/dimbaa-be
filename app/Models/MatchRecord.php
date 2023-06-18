0175dda2589dba54361e079f4ff529cedf5df56f
jameskaydev <75008974+jameskaydev@users.noreply.github.com>
2023-06-18 12:05:27 +0100

    Update MatchRecord.php

--- app/Models/MatchRecord.php
+++ app/Models/MatchRecord.php
@@ -20,6 +20,7 @@ class MatchRecord extends Model
         'round',
         'date'
     ];
+
     public function user(){
         return $this->belongsTo(User::class,'user_id')->withDefault([]);
     }
@@ -27,12 +28,29 @@ class MatchRecord extends Model
         return $this->belongsTo(Tournament::class,'tournament_id')->withDefault([]);
     }
     public function hometeam(){
-        return $this->belongsTo(Team::class,'home_team')->withDefault([]);
+        return $this->belongsTo(Team::class,'home_team_id')->withDefault([]);
     }
     public function awayteam(){
-        return $this->belongsTo(Team::class,'away_team')->withDefault([]);
+        return $this->belongsTo(Team::class,'away_team_id')->withDefault([]);
     }
     public function stadium(){
-        return $this->belongsTo(Stadium::class,'stadium')->withDefault([]);
+        return $this->belongsTo(Stadium::class,'stadium_id')->withDefault([]);
+    }
+    public function assigned(){
+        return $this->belongsTo(MatchOfficialAssistant::class,'away_team_id')->withDefault([]);
+    }
+
+    protected $appends = ['forteam','kick_off_time','official_assistant'];
+    public function getForTeamAttribute()
+    {
+        return Team::where('id',$this->away_team_id)->value('name');
+    }
+    public function getPrematchAttribute()
+    {
+        return PreMatchReport::where('match_id',$this->id)->value('kick_off_time');
+    }
+    public function getOfficialAssistantAttribute()
+    {
+        return MatchOfficialAssistant::where('match_id',$this->id)->value('commissioner');
     }
 }
