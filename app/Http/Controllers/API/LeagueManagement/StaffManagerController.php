756fbe0185e9599989070cedff501f93da7d4962
jameskaydev <75008974+jameskaydev@users.noreply.github.com>
2023-06-21 12:54:49 +0100

    Update StaffManagerController.php

--- app/Http/Controllers/API/LeagueManagement/StaffManagerController.php
+++ app/Http/Controllers/API/LeagueManagement/StaffManagerController.php
@@ -13,19 +13,9 @@ class StaffManagerController extends Controller
     {
         $staff = Staffs::where('team_id',Auth::user()->team_id)->get();
 
-        $res = [];
-        foreach($staff as $s){
-            $res['job_description'][$s['id']] = $s['jobdescription'];
-            $res['firstName'][$s['id']] = $s['firstName'];
-            $res['middleName'][$s['id']] = $s['middleName'];
-            $res['lastName'][$s['id']] = $s['lastName'];
-            $res['staffPic'][$s['id']] = $s['staffPic'];
-            $res['signature'][$s['id']] = $s['signature'];
-        }
-
         return response()->json([
             'success' => true,
-            'Content' => $res
+            'Content' => $staff
         ], 200);
     }
 }
